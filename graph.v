fcbaf9cdc9812bb19323be677cfb5bed92ee4664
Devin Jean <emailcruzjean@yahoo.com>
2024-06-28 06:56:37 -0500

    faster proofs

--- graph.v
+++ graph.v
@@ -182,23 +182,50 @@ Definition errld {G : graph} (D : V G -> Prop) := closed_dominating 3 D /\ self_
 (* ------------------------------------------------------------------------------------ *)
 
 Theorem old_is_odom : forall (G : graph) (D : V G -> Prop), old D -> odom D.
-Proof. firstorder. Qed.
+Proof. unfold old, odom. intros. destruct H as [H _]. exact H. Qed.
 
 Theorem redold_is_old : forall (G : graph) (D : V G -> Prop), redold D -> old D.
-Proof. firstorder. Qed.
+Proof. unfold redold, old. intros. destruct H as [H1 H2]. split. clear H2. firstorder. clear H1. firstorder. Qed.
 
 Theorem detold_is_redold : forall (G : graph) (D : V G -> Prop), detold D -> redold D.
 Proof.
     unfold detold, redold, sharp_open_distinguishing, open_distinguishing.
-    intros. destruct H as [H1 H2]. split. exact H1. intros. apply (H2 u v) in H. firstorder.
+    intros. destruct H as [H1 H2]. split. exact H1. clear H1. intros. apply (H2 u v) in H. clear H2. firstorder.
 Qed.
 
 Theorem errold_is_detold : forall (G : graph) (D : V G -> Prop), errold D -> detold D.
 Proof.
     unfold errold, detold, open_distinguishing, sharp_open_distinguishing, sharp_open_distinguished.
-    intros. destruct H as [H1 H2]. split. firstorder. intros. apply (H2 u v) in H. clear H1. clear H2.
+    intros. destruct H as [H1 H2]. split. clear H2. firstorder. clear H1. intros. apply (H2 u v) in H. clear H2.
     destruct H as [a [A H]]. destruct H as [b [B H]]. destruct H as [c [C _]].
     rewrite sym_sub in A. rewrite sym_sub in B. rewrite sym_sub in C.
     destruct A as [[A | A] Da], B as [[[B | B] Db] Uab], C as [[[[C | C] Dc] Uac] Ubc].
     all: firstorder.
 Qed.
+
+(* ------------------------------------------------------------------------------------ *)
+
+Theorem ic_is_dom : forall (G : graph) (D : V G -> Prop), ic D -> dom D.
+Proof. unfold ic, dom. intros. destruct H as [H _]. exact H. Qed.
+
+Theorem redic_is_ic : forall (G : graph) (D : V G -> Prop), redic D -> ic D.
+Proof. unfold redic, ic. intros. destruct H as [H1 H2]. split. clear H2. firstorder. clear H1. firstorder. Qed.
+
+Theorem detic_is_redic : forall (G : graph) (D : V G -> Prop), detic D -> redic D.
+Proof.
+    unfold detic, redic, sharp_closed_distinguishing, closed_distinguishing.
+    intros. destruct H as [H1 H2]. split. exact H1. clear H1. intros. apply (H2 u v) in H. clear H2. firstorder.
+Qed.
+
+Theorem erric_is_detic : forall (G : graph) (D : V G -> Prop), erric D -> detic D.
+Proof.
+    unfold erric, detic, closed_distinguishing, sharp_closed_distinguishing, sharp_closed_distinguished.
+    intros. destruct H as [H1 H2]. split. clear H2. firstorder. clear H1. intros. apply (H2 u v) in H. clear H2.
+    destruct H as [a [A H]]. destruct H as [b [B H]]. destruct H as [c [C _]].
+    rewrite sym_sub in A. rewrite sym_sub in B. rewrite sym_sub in C.
+    destruct A as [[A | A] Da], B as [[[B | B] Db] Uab], C as [[[[C | C] Dc] Uac] Ubc].
+    all: firstorder.
+Qed.
+
+(* ------------------------------------------------------------------------------------ *)
+
